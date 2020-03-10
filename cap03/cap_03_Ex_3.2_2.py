e6d984beb7b6ff10b682c246321080cdf3ca6f35
Emanuelvss13 <61998824+Emanuelvss13@users.noreply.github.com>
2020-03-10 10:37:09 -0300

    Add files via upload

---
+++ cap03/cap_03_Ex_3.2_2.py
@@ -0,0 +1,11 @@
+# 2
+
+def do_tw(f, g):
+    f(g)
+    f(g)
+
+def print_g(g):
+    print(g)
+
+
+do_tw(print_g, 'bolsonaro')
---
+++ cap03/cap_03_Ex_3.2_3.py
@@ -0,0 +1,3 @@
+# 3
+
+# Esta função atribui o argumento a um parâmetro. Quando a função é chamada, ela exibe o valor do parâmetro (seja qual for) duas vezes.
