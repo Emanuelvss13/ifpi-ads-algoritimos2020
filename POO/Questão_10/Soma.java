e78002c902d14d64a44aa71c1d50311245068f0e
Emanuelvss13 <61998824+Emanuelvss13@users.noreply.github.com>
2020-12-08 12:05:38 -0300

    Add files via upload

---
+++ POO/Questão_10/Soma.java
@@ -0,0 +1,8 @@
+
+public class Soma {
+	int n1, n2;
+	
+	int obterSoma() {
+		return n1 + n2;
+	}
+}
---
+++ POO/Questão_10/TesteSoma.java
@@ -0,0 +1,10 @@
+
+public class TesteSoma {
+	public static void main(String[] args) {
+		Soma so = new Soma();
+		so.n1 = 5;
+		so.n2 = 9;
+		
+		System.out.println(so.obterSoma());
+	}
+}
