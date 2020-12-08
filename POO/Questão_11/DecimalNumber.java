67a4af2f6f800d7943c8fc56d327375f7ad84f0c
Emanuelvss13 <61998824+Emanuelvss13@users.noreply.github.com>
2020-12-08 12:17:59 -0300

    Add files via upload

---
+++ POO/Questão_11/DecimalNumber.java
@@ -0,0 +1,12 @@
+
+public class DecimalNumber {
+	double number;
+	
+	int interge() {
+		return (int)number;
+	}
+	
+	double decimal() {
+		return number - (int)number;
+	}
+}
---
+++ POO/Questão_11/TesteDecimalNumber.java
@@ -0,0 +1,15 @@
+
+public class TesteDecimalNumber {
+	public static void main(String[] args) {
+		DecimalNumber dn = new DecimalNumber();
+		
+		dn.number = 3.56;
+		
+		System.out.println(dn.interge());
+		
+		System.out.println(dn.decimal());
+		
+		
+		
+	}
+}
