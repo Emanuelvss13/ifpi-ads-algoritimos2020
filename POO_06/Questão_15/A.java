c2032f3f299fd6a243999e75aabe65321380ab45
Emanuelvss13 <61998824+Emanuelvss13@users.noreply.github.com>
2021-01-20 21:59:50 -0300

    Add files via upload

---
+++ POO_06/Questão_15/A.java
@@ -0,0 +1,15 @@
+public class A {
+	protected int atributo1;
+	
+	void setAtributo1(int n) {
+		this.atributo1 = n;
+	}
+	
+	int getAtributo1() {
+		return atributo1;
+	}
+	
+	public String metodo1() {
+		return "metodo 1, classe A";
+	}
+}
